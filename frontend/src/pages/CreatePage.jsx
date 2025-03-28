6e438380bf6edb28c8baae197de7ae93375ea3d2
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-03-28 09:31:24 +0530

    modified the create prompt page

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import { Box, Button, Container, Heading, Input, VStack } from '@chakra-ui/react'
+import React, { useState } from 'react'
+import { useColorModeValue } from '../components/ui/color-mode'
 
 const CreatePage = () => {
+
+  const [newPrompt, setNewPrompt] = useState(
+    {
+      name : "",
+      prompt : "",
+      image : "",
+    }
+  )
+
+  const handleAddPrompt = () => {
+    console.log(newPrompt);
+    
+  }
+
   return (
-    <div>CreatePage</div>
+    <Container
+      maxW={"sm"}
+    >
+      <VStack
+        borderSpacing={8}
+      >
+        <Heading as={'h1'} size={'2xl'} textAlign={"center"} marginBottom={'8'} >
+          Create a Prompt
+        </Heading>
+        <Box
+          w={'full'}
+          bg={useColorModeValue("white", "grey.800")}
+          p={6}
+          rounded={'lg'}
+          shadow={'md'}
+        >
+          <VStack 
+            spacing={8}
+          >   
+          <Input
+            placeholder="Enter your prompt's name here"
+            name='name'
+            value={newPrompt.name}
+            onChange={(e) => setNewPrompt({...newPrompt, name:e.target.value})}
+          />
+          <Input
+            placeholder="Enter your prompt here"
+            name='prompt'
+            value={newPrompt.prompt}
+            onChange={(e) => setNewPrompt({...newPrompt, prompt:e.target.value})}
+          />
+          <Input
+            placeholder="Paste Your Image URL here"
+            name='image'
+            value={newPrompt.image}
+            onChange={(e) => setNewPrompt({...newPrompt, image:e.target.value})}
+          />
+          <Button colorScheme='blue' onClick={handleAddPrompt} w={'full'}>Create Prompt</Button>
+          </VStack>
+        </Box>
+      </VStack>
+    </Container>
   )
 }
 
