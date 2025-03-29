6ab30142fd8bbefd6d883dd4bfe256c6a31d1917
Jeffrey-joe-mathews <jeffreyjoemathews@gmail.com>
2025-03-29 22:45:11 +0530

    project completed

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 const app = express(); // creating an instance of express
+import path from 'path';
 
 import { connectDB } from './config/db.js';
 
@@ -10,10 +11,19 @@ import promptRoutes from './routes/prompt.route.js';
 
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json()); // middleware
 
 app.use('/api/prompts', promptRoutes)
 
+if(process.env.NODE_ENV === "production") {
+ app.use(express.static(path.join(__dirname, "/frontend/dist")));   
+
+ app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "imdex.html"));
+ })
+}
 
 app.listen (PORT, async () => {
     await connectDB();
